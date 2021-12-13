8e6dcb33f2668b3076faa3f37231c1e5882fe3da
UrbanoG2 <giuseppe.16.urbano@gmail.com>
2021-12-13 17:16:32 +0100

    selection

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 
+// Milestone 1
+// Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell'icona e l'icona stessa.
 
 
 // Milestone 2
@@ -14,10 +16,7 @@
 // 2- popolare le options della select della milestone 3 dinamicamente.
 
 
-// Consigli del giorno
-// Come sempre, iniziamo prima di tutto dall'analisi e comprensione della consegna. Scomponiamo il problema in micro-passaggi logici che solamente in un secondo momento trasformeremo in codice.
-// Le icone presenti nella struttura dati fanno riferimento alla nota libreria Font Awesome, perciò come prima cosa assicuriamoci di aver inserito il link alla cdn nell'head della pagina. Dopodiché, basandoci sul codice di riferimento nel sito di Font Awesome, analizziamo come è formato il tag <i> di un'icona qualsiasi, in particolare focalizziamoci sulle classi. Come possiamo usare i dati presenti nella nostra struttura dati per creare l'elemento html nel modo corretto e visualizzare l'icona in pagina?
-// Inizialmente può essere sufficiente stampare dei semplici div, senza alcuno stile, con all'interno l'icona e uno span con il nome. Solamente quando la parte logica è completa, ci dedichiamo al css.
+
 
 
 const boxArr = [
@@ -144,15 +143,12 @@ createElement (boxArr, container);
 function createElement (array, container) {
     for (let i = 0; i < array.length; i++) {
         
-        
 
         const container = document.getElementById ('container');
 
         const object = array[i];
 
         let iconName = object.family + ' ' + object.prefix + object.name;
-
-        console.log(iconName);
 
         const containedElement = `
         
@@ -167,12 +163,28 @@ function createElement (array, container) {
         </div>
 
         `
-
         
         container.innerHTML += containedElement;
     }
 
+// Milestone 3
+// Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
+
+
+
 }
 
 
+//filtro le categorie
 
+const animalFiltered = boxArr.filter(element => element.type === 'animal' );
+
+const vegetableFiltered = boxArr.filter(element => element.type === 'vegetable' );
+
+const userFiltered = boxArr.filter(element => element.type === 'user' );
+
+console.log(animalFiltered, vegetableFiltered, userFiltered);
+
+const selection = 
+
+console.log(selection);
